f592bcccaf0668a0968cc6098923a2b913c01022
FeLL1kS <oleg5420@yandex.ru>
2020-04-07 02:28:11 +0300

    Initial commit

---
+++ Library/Library.DTO/AuthorDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Library.DTO
+{
+    /// <summary>
+    /// Класс - автор
+    /// </summary>
+    public class AuthorDto
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// ФИО
+        /// </summary>
+        public string FullName { get; set; }
+    }
+}
